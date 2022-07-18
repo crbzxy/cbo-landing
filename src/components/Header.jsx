3edad2ed7fc69be0225b216578acc300a341537c
Carlos Boyzo <carlos.boor@gmail.com>
2022-07-18 04:05:23 -0500

    ajuste

---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+//import reactLogo from '../assets/react.svg'
+import { useState } from 'react'
+
+function Header() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header className='contenedor h-screen'>
+        <div className='flex'>
+          <a href='https://vitejs.dev' target='_blank'>
+            <img src='/vite.svg' className='logo' alt='Vite logo' />
+          </a>
+          
+        </div>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+
+        <div className='card'>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+
+          <p className='pt-3'>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className='read-the-docs'>
+          Landing page from Carlos Boyzo
+        </p>
+      </header>
+    </>
+  );
+}
+
+export default Header;
